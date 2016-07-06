d781cdc39ebf5da20dd2298c680d479490811767
Pierre Martin <pierre@occitech.fr>
2016-07-06 07:32:28 +0200

    Add a basic displayed Pomodoro timer

--- src/components/Pomodoro/index.js
+++ src/components/Pomodoro/index.js
@@ -1,7 +1,73 @@
 import React from 'react'
+import {Button, Container, Donut} from 'rebass'
+import {createEventHandler, mapPropsStream} from 'recompose'
+import {Observable} from 'rxjs'
 
-export default () => <div>
+const POMODORO_DURATION_IN_SEC = 25 * 60
+
+// see http://stackoverflow.com/a/14760377
+const padTimePart = (part) => ('00' + part).slice(-2)
+const HumanReadableTime = ({seconds}) => {
+  const minutes = Math.floor(seconds / 60)
+  const secondsOfMinute = (seconds % 60)
+
+  return <span>{padTimePart(minutes)}:{padTimePart(secondsOfMinute)}</span>
+}
+
+const Pomodoro = (props) => <div>
   <h2>Pomodoro</h2>
 
-  Je suis un pomodoro
+  <Container>
+    <Donut
+      color='primary'
+      size={256}
+      strokeWidth={32}
+      value={props.timeElapsed / POMODORO_DURATION_IN_SEC}
+    >
+      <HumanReadableTime seconds={props.timeElapsed} />
+    </Donut>
+  </Container>
+
+  <Container>
+    <Button
+      backgroundColor='success'
+      color='white'
+      pill
+      rounded
+      onClick={props.start}
+    >
+      Start
+    </Button>
+    <Button
+      backgroundColor='error'
+      color='white'
+      pill
+      rounded
+      onClick={props.stop}
+    >
+      Stop
+    </Button>
+  </Container>
 </div>
+
+const enhance = mapPropsStream((props$) => {
+  const { handler: start, stream: start$ } = createEventHandler()
+  const { handler: stop, stream: stop$ } = createEventHandler()
+
+  const timeElapsed$ = start$
+    .flatMap(() => Observable
+      .interval(1000)
+      .take(POMODORO_DURATION_IN_SEC + 1)
+      .takeUntil(stop$)
+    )
+    .startWith(0)
+
+  return props$.combineLatest(timeElapsed$, (props, timeElapsed) => ({
+    ...props,
+    timeElapsed,
+    start,
+    stop
+  }))
+})
+
+export default enhance(Pomodoro)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { setObservableConfig } from 'recompose'
+import rxjsconfig from 'recompose/rxjsObservableConfig'
 
 import App from './components/App'
 
+setObservableConfig(rxjsconfig)
 render(
   <App />,
   document.getElementById('app')
